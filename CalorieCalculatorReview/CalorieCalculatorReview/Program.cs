94fc2ba052d84779a7a707321b92107355c8e929
Edward Louie <edwardlouie29@ou.edu>
2020-02-25 23:43:51 -0600

    Calorie Counter

---
+++ CalorieCalculatorReview/CalorieCalculatorReview/Program.cs
@@ -0,0 +1,28 @@
+﻿//Ed Louie
+using System;
+
+namespace CalorieCalculatorReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double calorieGoal;
+            double caloriesBurned = 0;
+            double min = 0;
+
+            Console.WriteLine("How many calories do you want to burn?");
+            calorieGoal = Convert.ToDouble(Console.ReadLine());
+
+            while (caloriesBurned <= calorieGoal)
+            {
+                min += 1;
+                caloriesBurned += 3.9;
+
+                Console.WriteLine($"After {min} minute(s), you have burned {caloriesBurned.ToString("N2")}.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
