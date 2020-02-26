f922c90092b9ae3f80ebfec19ea61d30730df1b1
Edward Louie <edwardlouie29@ou.edu>
2020-02-25 19:20:04 -0600

    Accumulators Review

---
+++ Accumulators/Accumulators/Program.cs
@@ -0,0 +1,29 @@
+﻿//Ed Louie
+using System;
+
+namespace Accumulators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add all numbers from 1 - 100 and multiply all numbers from 1 - 100
+
+            int counter;
+            double multiplication = 1;
+            int sum = 0;
+
+            for (counter = 1; counter <= 100; counter++)
+            {
+                sum += counter;
+                multiplication *= counter;
+            }
+
+            Console.WriteLine($"Summation value from 1 - 100 is {sum.ToString("N0")}");
+            Console.WriteLine($"Multiplication value from 1 - 100 is {multiplication.ToString("N0")}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
