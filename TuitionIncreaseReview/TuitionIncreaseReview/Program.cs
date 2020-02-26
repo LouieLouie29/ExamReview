b0d7528ee1c4d9082ed9df14e1400e3d74be97c3
Edward Louie <edwardlouie29@ou.edu>
2020-02-25 23:59:16 -0600

    Tution Increase Calculator

---
+++ TuitionIncreaseReview/TuitionIncreaseReview/Program.cs
@@ -0,0 +1,34 @@
+﻿//Ed Louie
+using System;
+
+namespace TuitionIncreaseReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              double tuition = 12000;
+              double tuitionInc = 0;
+              double increase = 0.05;
+              string msg;
+
+              for (int years = 1; years <= 7; years++)
+              {
+                  if (years >= 1)
+                  {
+                      tuitionInc = tuition * increase;
+                  }
+
+                  tuition = tuitionInc + tuition;
+
+                msg = ($"Your tuition after {years.ToString()} will be {tuition.ToString("C2")}.");
+                Console.WriteLine(msg);
+              }
+              
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
